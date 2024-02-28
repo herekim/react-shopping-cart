e47a96404a4ef7cee7259d9aa8e6ebc7d90a1e5d
herekim <khere220@gmail.com>
2024-02-28 13:47:40 +0900

    feat: URL 수정

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -6,13 +6,7 @@ const ProductList = () => {
 
   return (
     <>
-      <a
-        href="
-        
-https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/
-
-"
-      >
+      <a href="https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_dp=connectingapp%3A%2F%2F&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
         링크 wLqS
       </a>
       <a href="https://connecting.onelink.me/Jd5O?af_js_web=true&af_ss_ver=2_7_3&pid=report&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
