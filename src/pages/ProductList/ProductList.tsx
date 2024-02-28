12144da9cdc60bbb684b7da3ff31f75f10128256
herekim <khere220@gmail.com>
2024-02-28 17:12:39 +0900

    feat: UI 수정

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -6,7 +6,7 @@ const ProductList = () => {
 
   return (
     <>
-      <div className="gap-10">
+      <div className="flex-col-center gap-10">
         <a href="https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_dp=connectingapp%3A%2F%2F&af_force_deeplink=true&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
           링크 wLqS (URI Scheme)
         </a>
