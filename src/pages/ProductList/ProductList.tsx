50ce8a30b89a5263004a225380aa0c406cf9fc97
herekim <khere220@gmail.com>
2024-02-28 16:28:04 +0900

    feat: URI 스킴 변경

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -7,7 +7,7 @@ const ProductList = () => {
   return (
     <>
       <div className="gap-10">
-        <a href="https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_dp=connecting%3A%2F%2Fmainactivity&af_force_deeplink=true&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
+        <a href="https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_dp=connectingapp%3A%2F%2F&af_force_deeplink=true&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
           링크 wLqS (URI Scheme)
         </a>
         <a href="https://connecting.onelink.me/wLqS?af_js_web=true&af_ss_ver=2_7_3&pid=report&deep_link_value=reportAnalysis&af_ss_ui=true&af_channel=https://hq1.appsflyer.com/">
